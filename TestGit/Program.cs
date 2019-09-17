4015574aaf8f65d787dc52890b935ddd10e57d44
mrnooffline <49479874+mrnooffline@users.noreply.github.com>
2019-09-17 11:17:10 +0700

    initial

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -10,6 +10,8 @@ namespace TestGit
     {
         static void Main(string[] args)
         {
+            Console.Write("Hello guys! This is TestGit project.");
+            Console.Read();
         }
     }
 }
